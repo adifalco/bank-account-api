37b1e4a2e97c4de5a2aa72299d7402177eb60a7c
adifalco <andresdifalco@gmail.com>
2017-09-12 19:06:14 +0200

    Spring Boot application and Swagger2 configuration setup

---
+++ src/main/java/com/coding/example/bank_account_api/MainApplication.java
@@ -0,0 +1,38 @@
+package com.coding.example.bank_account_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    public Docket docket()
+    {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(getClass().getPackage().getName()))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(generateApiInfo());
+    }
+
+
+    private ApiInfo generateApiInfo()
+    {
+        return new ApiInfo("Coding Example for a Rest API", "This service is to show up how I'd implement a Spring Boot based Rest API", "Version 1.0",
+                "urn:tos", "Andrés Di Falco", "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+    }
+}
---
+++ src/test/java/com/coding/example/bank_account_api/MainApplicationTest.java
@@ -0,0 +1,15 @@
+package com.coding.example.bank_account_api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MainApplication.class)
+public class MainApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
